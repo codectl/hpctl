a75387829a7dfbbe62f99fa3a61e49381d5e8455
Renato Damas <me@renatodamas.com>
2022-12-13 20:34:30 -0500

    chore: isolate package metadata

--- src/hpctl/__init__.py
+++ src/hpctl/__init__.py
@@ -1,6 +1,11 @@
-from importlib import metadata
-
 from .cli import cli
+from .__metadata__ import __title__, __description__, __version__, __author__, __license__
 
-__all__ = (cli,)
-__version__ = metadata.version("hpctl")
+__all__ = (
+    "cli",
+    "__title__",
+    "__description__",
+    "__version__",
+    "__author__",
+    "__license__",
+)
